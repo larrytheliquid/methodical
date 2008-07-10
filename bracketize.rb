3bea64561f396f057591908834e787311722ace1
Larry Diehl <larry@izea.com>
2008-07-10 12:35:01 -0400

    Added initial specific implementation

--- bracketize.rb
+++ bracketize.rb
@@ -1,42 +1,83 @@
-# Ever wanted Ruby to have mandator parentheses for method calls...
-# and have those method calls actually use brackets instead of
-# parantheses? Probably not, but here's how to do it.
 class Calculator
-  def initialize(base)
-    @base = base
+  attr_reader :base
+  
+  def self.new
+    o = method_holder
+    instance_of_self = super 
+    (class << o; self; end).send(:define_method, :[]) do |base|
+      instance_of_self.instance_variable_set(:@base, base)
+      instance_of_self
+    end
+    o
   end
   
-  # def self.new
-  #   o = Object.new
-  #   def o.[](base) initialize(base) end
-  #   o
-  # end
-  
   def add
-    o = Object.new
+    o = self.class.method_holder
     def o.[](a, b) a + b end
     o
   end
   
   # make this use add
   def multiply
-    o = Object.new
+    o = self.class.method_holder
     def o.[](a, b) a * b end
     o
   end
   
-  def add_then_multiply
-    
-  end
-  
 private
   
-  def new_object
-    
+  def self.method_holder
+    Object.new
   end
-
 end
 
-calc = Calculator.new(10)
-puts calc.add[3, 4] # => 7
-puts calc.multiply[3, 4] # => 12
+describe Calculator do
+  before(:each) do
+    @calculator = Calculator.new[10]
+  end
+  
+  describe Calculator, "#new" do        
+    describe "called with brackets" do
+      it "should set base" do
+        Calculator.new[10].base.should == 10
+      end
+    end
+
+    describe "called without brackets" do
+      it "should return a reference to the method that can be called with brackets" do        
+        new = Calculator.new
+        new[10].base.should == 10
+      end
+    end
+  end
+
+  describe Calculator, "#add" do  
+    describe "called with brackets" do
+      it "should return the sum of the values" do
+        @calculator.add[4, 7].should == 11
+      end
+    end
+
+    describe "called without brackets" do
+      it "should return a reference to the method that can be called with brackets" do
+        add = @calculator.add
+        add[4, 7].should == 11
+      end
+    end
+  end
+
+  describe Calculator, "#multiply" do
+    describe "called with brackets" do
+      it "should return the product of the values" do
+        @calculator.multiply[4, 7].should == 28
+      end
+
+      describe "called without brackets" do
+        it "should return a reference to the method that can be called with brackets" do
+          multiply = @calculator.multiply
+          multiply[4, 7].should == 28
+        end
+      end
+    end
+  end
+end
