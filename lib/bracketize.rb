3e5b4d6e9f8fd566f8e4e21210e233842d3e8fb6
Larry Diehl <larry@izea.com>
2008-07-10 23:58:31 -0400

    Got rid of unnecessary closures

--- lib/bracketize.rb
+++ lib/bracketize.rb
@@ -1,12 +1,12 @@
 module Bracketize
   def bracketize(*method_names)
     method_names.each do |method_name|
-      class_eval do
-        alias_method(:"#{method_name}_without_bracketize", method_name)
-        define_method(method_name) do
-          method(:"#{method_name}_without_bracketize")
+      class_eval %{
+        alias #{method_name}_without_bracketize #{method_name}
+        def #{method_name}
+          method('#{method_name}_without_bracketize')
         end
-      end
+      }
     end
   end
 end
